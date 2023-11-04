bfa31d2dceb90efcb769a152fa7e9bbb3eb1004d
Avaz <elonarm21@gmail.com>
2023-11-04 16:01:09 +0500

    folderlarga ajratish

--- src/app/App.js
+++ src/app/App.js
@@ -1,22 +1,12 @@
-import React, {useLayoutEffect, useState} from 'react';
+import React from 'react';
+import UseLayoutEffect from "../components/useLayoutEffect/UseLayoutEffect";
+import UseReducer from "../components/useReducer/UseReducer";
 
 const App = () => {
-    const [random, setRandom] = useState(0)
-    useLayoutEffect(() => {
-        if (random === 0) {
-            setRandom(Math.floor(Math.random() * 100))
-        }
-    }, [random]);
     return (
         <div className={"app"}>
-            <div className="container">
-                <div>
-                    <h1>Random: <span>{random}</span></h1>
-                    <button className={"btn btn-sm btn-outline-primary"} onClick={() => setRandom(0)}>
-                        Generate
-                    </button>
-                </div>
-            </div>
+            <UseLayoutEffect />
+            <UseReducer />
         </div>
     );
 };
