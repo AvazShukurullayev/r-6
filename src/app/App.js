079959ae738b3dcdebcd6e2a3d6e157b262e5c4a
Avaz <elonarm21@gmail.com>
2023-11-04 15:47:47 +0500

    useEffect bilan ishlash number generate qilish

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const App = () => {
+    const [random, setRandom] = useState(0)
+    useEffect(() => {
+        if (random === 0) {
+            setRandom(Math.floor(Math.random() * 100))
+        }
+    }, [random]);
     return (
         <div className={"app"}>
-
+            <div className="container">
+                <div>
+                    <h1>Random: <span>{random}</span></h1>
+                    <button className={"btn btn-sm btn-outline-primary"} onClick={() => setRandom(0)}>
+                        Generate
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
